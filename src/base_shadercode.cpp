223e7ebcf405a3cdca148439d150855bad9f6e79
DietfridMali <karx11erx@googlemail.com>
2025-08-07 21:55:59 +0200

    removed some debug output and obsolete commented out code

--- src/base_shadercode.cpp
+++ src/base_shadercode.cpp
@@ -29,14 +29,11 @@ BaseShaderCode::BaseShaderCode() {
 
 void BaseShaderCode::AddShaders(ManagedArray<const ShaderSource*>& shaderSource) {
     for (const ShaderSource* source : shaderSource) {
-        fprintf(stderr, "creating shader '%s'", (const char*) source->m_name);
         Shader* shader = new Shader(source->m_name);
         if (shader->Create(source->m_vs, source->m_fs))
             m_shaders[source->m_name] = shader;
         else
-            fprintf(stderr, " - failed");
-        fprintf(stderr, "\n");
-        //m_shaders.Insert(ss.m_name, s);
+            fprintf(stderr, "creating shader '%s' failed\n", (const char*) source->m_name);
     }
 }
 
